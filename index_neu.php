02e9439117db07ee0779944fe937833b7701004c
ellayel <nora.kasper@mmp.htwchur.ch>
2016-11-11 09:17:50 +0100

    Änderungen in CSS und Index Neu

---
+++ index_neu.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="De">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--Mobile First-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Wissensplatz Login</title>
+    <!-- Bootstrap CSS-->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- Eigenes CSS -->
+    <link href="css/main.css" rel="stylesheet">
+    <!-- Google Schrift -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700,700italic,300italic' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+<!-- Login Code -->
+<div class="container">
+        <div class="card card-container">
+            <!-- <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" /> -->
+            <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+            <p id="profile-name" class="profile-name-card"></p>
+            <form class="form-signin">
+                <span id="reauth-email" class="reauth-email"></span>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <div id="remember" class="checkbox">
+                    <label>
+                        <input type="checkbox" value="remember-me"> Remember me
+                    </label>
+                </div>
+                <button class=".btn.btn-signin" type="submit">Sign in</button>
+            </form><!-- /form -->
+            <a href="#" class="forgot-password">
+                Forgot the password?
+            </a>
+        </div><!-- /card-container -->
+    </div><!-- /container -->
+<!-- Ende Login Code -->
+
+
+
+
+
+
+
+
+
+
+    <!-- jQuery -->
+    <script  src="js/jquery.js"></script>
+    <!--Bootstrap-->
+    <script src="js/bootstrap.min.js"></script>
+
+    <script src="js/dropzone.js"></script>
+
+    <script>
+    $( document ).ready(function() {
+    // DOM ready
+
+    // Test data
+    /*
+     * To test the script you should discomment the function
+     * testLocalStorageData and refresh the page. The function
+     * will load some test data and the loadProfile
+     * will do the changes in the UI
+     */
+    // testLocalStorageData();
+    // Load profile if it exits
+    loadProfile();
+});
+
+/**
+ * Function that gets the data of the profile in case
+ * thar it has already saved in localstorage. Only the
+ * UI will be update in case that all data is available
+ *
+ * A not existing key in localstorage return null
+ *
+ */
+function getLocalProfile(callback){
+    var profileImgSrc      = localStorage.getItem("PROFILE_IMG_SRC");
+    var profileName        = localStorage.getItem("PROFILE_NAME");
+    var profileReAuthEmail = localStorage.getItem("PROFILE_REAUTH_EMAIL");
+
+    if(profileName !== null
+            && profileReAuthEmail !== null
+            && profileImgSrc !== null) {
+        callback(profileImgSrc, profileName, profileReAuthEmail);
+    }
+}
+
+/**
+ * Main function that load the profile if exists
+ * in localstorage
+ */
+function loadProfile() {
+    if(!supportsHTML5Storage()) { return false; }
+    // we have to provide to the callback the basic
+    // information to set the profile
+    getLocalProfile(function(profileImgSrc, profileName, profileReAuthEmail) {
+        //changes in the UI
+        $("#profile-img").attr("src",profileImgSrc);
+        $("#profile-name").html(profileName);
+        $("#reauth-email").html(profileReAuthEmail);
+        $("#inputEmail").hide();
+        $("#remember").hide();
+    });
+}
+
+/**
+ * function that checks if the browser supports HTML5
+ * local storage
+ *
+ * @returns {boolean}
+ */
+function supportsHTML5Storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
+
+/**
+ * Test data. This data will be safe by the web app
+ * in the first successful login of a auth user.
+ * To Test the scripts, delete the localstorage data
+ * and comment this call.
+ *
+ * @returns {boolean}
+ */
+function testLocalStorageData() {
+    if(!supportsHTML5Storage()) { return false; }
+    localStorage.setItem("PROFILE_IMG_SRC", "//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" );
+    localStorage.setItem("PROFILE_NAME", "César Izquierdo Tello");
+    localStorage.setItem("PROFILE_REAUTH_EMAIL", "oneaccount@gmail.com");
+}
+</script>
+
+
+
+</body>
+
+</html>
